cb15c7876de3f1022c533eb83704c20022666562
djonzoumbek <guepelbekamis@gmail.com>
2024-11-02 01:26:23 +0000

    Annulation de la mise en ligne

--- src/blog_backend/settings.py
+++ src/blog_backend/settings.py
@@ -11,14 +11,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-import environ
-import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-env = environ.Env()
-env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
@@ -84,21 +80,18 @@ WSGI_APPLICATION = 'blog_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'blog0110',
-#         'USER': 'blog_user',
-#         'PASSWORD': 'blog_user',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
-
-
 DATABASES = {
-    'default' : dj_database_url.parse(env('DATABASE_URL')),
-}
+     'default': {
+         'ENGINE': 'django.db.backends.postgresql',
+         'NAME': 'blog0110',
+         'USER': 'blog_user',
+         'PASSWORD': 'blog_user',
+         'HOST': 'localhost',
+         'PORT': '5432',
+     }
+ }
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
